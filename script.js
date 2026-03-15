e552fc31ad5b3591908b94ebe3a9cd408e66a100
storhmrich <storhmrichhh@gmail.com>
2026-03-15 09:07:42 +0000

    update codes

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ title.innerText = "Staffing Request: " + service
 }
 
 }
+
+
+
+// Fade-in on scroll
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.1,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
